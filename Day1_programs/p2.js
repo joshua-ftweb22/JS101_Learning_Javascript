16d553ca66456abd7873555c7fbb94b2f27c79be
joshua-ftweb22 <you@example.com>
2022-10-11 22:27:01 +0530

    day1_programs

---
+++ Day1_programs/p2.js
@@ -0,0 +1,12 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+let name1="joshua";
+console.log(name1);
+let name2="jayaraj";
+console.log(name2);
+let name3="mary";
+console.log(name3);
---
+++ Day1_programs/p4.js
@@ -0,0 +1,16 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+console.log("#############################");
+console.log("Name    : J.Joshua");
+console.log("School  : Golden Bells School");
+console.log("Grade   : A+");
+console.log("Section : A");
+console.log("Roll NO : 220")
+console.log("----------------------------");
+console.log("| Subjects | Marks | Grade |");
+console.log("|----------|-------|-------|");
+console.log("| Maths    |  40   |   B   |");
+console.log("| English  |  50   |   A+  |");
+console.log("| Science  |  45   |   A   |");
+console.log("----------------------------");
+console.log("#############################");
